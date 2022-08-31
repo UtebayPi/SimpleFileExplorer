8b63cf51530e0bd0da2b82216bcd969fc88cb31c
UtebayPi <persiutebay@gmail.com>
2022-08-31 21:30:33 +0600

    Сохраняю прогресс

--- app/src/main/java/com/utebaykazalm/simplefileexplorer/ui/TextFilesListFragment.kt
+++ app/src/main/java/com/utebaykazalm/simplefileexplorer/ui/TextFilesListFragment.kt
@@ -7,7 +7,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.utebaykazalm.simplefileexplorer.data.TextFile
